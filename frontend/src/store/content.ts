b3f155f425adf5a4a3e1d52f97b1c3beaae8e600
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2025-02-07 16:46:40 +0800

    sign up make friendly

--- frontend/src/store/content.ts
+++ frontend/src/store/content.ts
@@ -1,12 +1,10 @@
 import { create } from "zustand";
 
-// Define the type for the store's state
 interface ContentState {
   contentType: string;
-  setContentType: (type: string) => void; // You can type `type` more strictly if needed
+  setContentType: (type: string) => void;
 }
 
-// Create the zustand store with the defined types
 export const useContentStore = create<ContentState>((set) => ({
   contentType: "movie", // Initial state
   setContentType: (type) => set({ contentType: type }),
